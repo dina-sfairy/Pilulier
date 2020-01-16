0397029e94501445f43dbfb7d349baa10fa22f07
Unknown <jonathan-4.lacombe@polymtl.ca>
2020-01-16 10:12:42 -0500

    test

---
+++ Code_Pilulier/test_/Debug/test_.cpp.d
@@ -0,0 +1,15 @@
+C:\Users\jonat\AppData\Local\Temp\VMBuilds\test_\mega_atmega2560\Debug\test_.cpp.o: \
+ C:\Users\jonat\AppData\Local\Temp\VMBuilds\test_\mega_atmega2560\Debug\test_.cpp \
+ C:\Users\jonat\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.21\cores\arduino/arduino.h \
+ C:\Users\jonat\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.21\cores\arduino/binary.h \
+ C:\Users\jonat\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.21\cores\arduino/WCharacter.h \
+ C:\Users\jonat\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.21\cores\arduino/WString.h \
+ C:\Users\jonat\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.21\cores\arduino/HardwareSerial.h \
+ C:\Users\jonat\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.21\cores\arduino/Stream.h \
+ C:\Users\jonat\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.21\cores\arduino/Print.h \
+ C:\Users\jonat\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.21\cores\arduino/Printable.h \
+ C:\Users\jonat\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.21\cores\arduino/USBAPI.h \
+ C:\Users\jonat\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.21\cores\arduino/Arduino.h \
+ C:\Users\jonat\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.21\variants\mega/pins_arduino.h \
+ c:\program\ files\ (x86)\microsoft\ visual\ studio\2019\community\common7\ide\extensions\cpwzuv43.p51\Micro\ Platforms\default\debuggers\VM_DBG/VM_DBG.h \
+ C:\Users\jonat\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.21\cores\arduino/Arduino.h
---
+++ Code_Pilulier/test_/test_.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		test_.ino
+ Created:	16/01/2020 10:11:29
+ Author:	jonat
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
