abf150c84c593111cfb77dc57f3f7fd4a3e10904
Unknown <jonathan-4.lacombe@polymtl.ca>
2020-01-21 17:43:58 -0500

    Test de distribution sur le montage

--- Code_Pilulier/Distribution/Distribution.ino
+++ Code_Pilulier/Distribution/Distribution.ino
@@ -17,31 +17,35 @@ void ActiverStepper2() {
 	ASstepper2.run();
 }
 int LED = 13;
-int x = 0;
-int pos = 0;
-int momentJournee = 0;
+int pos;
+int momentJournee;
 bool retour;
 int etape;
-int servoPin = 3;
+int servoPin;
 int homePosition;
-int posAjustement = (int)(100 / PI);
+int posAjustement;
 // Create a servo object 
 Servo Servo1;
 
 void setup() {
 	// Define the LED pin as Output
 	pinMode(LED, OUTPUT);
-	ASstepper2.setMaxSpeed(6000);
-	ASstepper2.setAcceleration(4000);
+	ASstepper2.setMaxSpeed(2000);
+	ASstepper2.setAcceleration(500);
+	ASstepper2.setCurrentPosition(0);
 	// Start the I2C Bus as Slave on address 11
 	Wire.begin(11);
 	// Attach a function to trigger when something is received.
 	Wire.onReceive(receiveEvent);
 	Wire.onRequest(requestEvent);
-	retour = false;
-	etape = 1;
+	retour = false; 
+	etape = 2;
+	pos = 0;
+	momentJournee = 0;
+	servoPin = 3;
+	posAjustement = (int)(100 / PI);
 	Servo1.attach(servoPin);
-	Servo1.write(0); // À ajuster en fonction de la position réelle pour que la trappe soit horizontale initialement
+	Servo1.write(163); // À ajuster en fonction de la position réelle pour que la trappe soit horizontale initialement
 	delay(500);
 	pinMode(6, INPUT);
 
@@ -49,23 +53,30 @@ void setup() {
 void receiveEvent(int bytes) {
 	//pos = 1600 * Wire.read();  // Pour les PIN MS1 et MS2 connectés au ground du EasyDriver, 400 steps = 1 tour
 	momentJournee = Wire.read();
-	retour = false;
+	Serial.println(momentJournee);
 	etape = 1;
+	// retour = false;
+	pos = 0;
+
 	switch (momentJournee)
 	{
 	case 1:
-		pos = 0;
+		retour = true;
 		break;
 	case 2:
 		pos = (int)(20 * 57 / PI); // Les calculs assument 1 tour = 400 steps
+		retour = false;
 		break;
 	case 3:
 		pos = (int)(20 * (57 + 63) / PI);
+		retour = false;
 		break;
 	case 4:
 		pos = (int)(20 * (57 * 2 + 63) / PI);
+		retour = false;
 		break;
 	default:
+		retour = true; // À cause des problèmes de communication, on ne veut pas entrer dasn la première boucle
 		break;
 	}
 	// Si MS1 et MS2 ne sont pas connectés au ground, 1600 steps = 1 tour
@@ -78,35 +89,34 @@ void receiveEvent(int bytes) {
 void requestEvent() {
 	
 	Wire.write(retour);
-
 }
 
 void loop() {
+
 	ASstepper2.run();
-	/*homePosition = digitalRead(6);
-	Serial.println(homePosition);
-	delay(10);*/
 	if (ASstepper2.distanceToGo() == 0) {
 
-		if (etape == 1) {
+		if (etape == 1 && !retour ) {
 
-			Servo1.write(90);
+			Servo1.write(Servo1.read() - 60);
 			delay(500);
-			Servo1.write(0);
+			Servo1.write(Servo1.read() + 60);
 			delay(500);
-			pos = -pos;
 			ASstepper2.setCurrentPosition(0);
-			ASstepper2.moveTo(pos);
+			ASstepper2.moveTo(-abs(pos));
+			pos = 0;
 			etape++;
 		}
 		else if (etape == 2) {
-
 			if (digitalRead(6) != 0) {
 				ASstepper2.setCurrentPosition(0);
 				ASstepper2.moveTo(-posAjustement);
 			}
 			else {
 				retour = true;
+				etape = 1;
+				momentJournee = 0;
+				pos = 0;
 			}
 		}
 
--- Code_Pilulier/Master/Master.ino
+++ Code_Pilulier/Master/Master.ino
@@ -1,51 +1,63 @@
 
 #include <TimedAction.h>
 #include <Wire.h>
+
 int nano_comp = 1;
-int nano_dist = 4;
+int nano_dist = 2;
 int test[9] = { 0,1,2,3,8,7,8, 1,8 };
 int pos;
+bool ready;
+int presentTime = 0;
+int chiffre;
 // Test de vérification
-
 TimedAction timedActionComp = TimedAction(3500, commanderCompartimentation);
 TimedAction timedActionDist = TimedAction(8000, commanderDistribution);
+
+int vect[10];
+const int size = sizeof(vect);
+int tab[size];
+
 
 void setup() {
 	// Start the I2C Bus as Master
 	Wire.begin();
 	pinMode(24, INPUT);
 	pos = 0;
+	presentTime = millis();
+	ready = false;
+	chiffre = 0;
+	
 }
 
 void loop() {
 	
 	//timedActionComp.check();
-	//timedActionDist.check();
-	delay(500);
-	timedActionComp.check();
+	timedActionDist.check();
 
 	
-
-	//Serial.println(nano_dist);
-	//Wire.requestFrom(11, 1);
-	//bool ready = false;
-	//while (Wire.available())    // slave may send less than requested
+	//if (millis() - presentTime > 2000)
 	//{
-	//	ready = Wire.read();
+	//	presentTime = millis();
+	//	Wire.requestFrom(11, 1);
+	//	ready = true;
+	//	while (Wire.available())    // slave may send less than requested
+	//	{
+	//		chiffre = Wire.read();
+	//	}
+	//	Serial.println(chiffre);
+	//	if (chiffre) {
+	//		Wire.beginTransmission(11);
+	//		Wire.write(nano_dist);
+	//		Wire.endTransmission();
+	//	}
 	//}
-	//Serial.println(ready);
 	
 
 }
 
 
 void commanderCompartimentation() {
-	/*if (nano_comp > 7) {
-		nano_comp = 1;
-	}
-	else if (nano_comp == 4) {
-		nano_comp = 8;
-	}*/
+	
 	nano_comp = test[pos];
 	pos++;
 	Serial.println(nano_comp);
@@ -59,6 +71,7 @@ void commanderCompartimentation() {
 }
 
 void commanderDistribution() {
+	Serial.println(nano_dist);
 	Wire.beginTransmission(11); // transmit to device #11
 	Wire.write(nano_dist);              // sends x 
 	Wire.endTransmission();    // stop transmitting
