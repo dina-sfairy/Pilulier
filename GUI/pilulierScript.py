8762dec31c9e1f810575df893b578615fdb0963f
Thierry Beiko <thierry.beiko@polymtl.ca>
2020-02-06 13:22:39 -0500

    changed COM

--- GUI/pilulierScript.py
+++ GUI/pilulierScript.py
@@ -41,7 +41,7 @@ class ApplicationPilulier:
         self.systemControl = 0
 
         # Démarer la communication avec l'arduino
-        self.serPort = serial.Serial('COM6', 115200)
+        self.serPort = serial.Serial('COM15', 115200)
         time.sleep(3)  # Il faut donner le temps au Arduino de reset
 
     def onDemarerClicked(self):
