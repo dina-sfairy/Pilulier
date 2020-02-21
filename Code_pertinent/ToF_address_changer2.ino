e0020d6131a76bcaa988425a8c649794c8197e43
Stoyan I. Asenov <43790661+Stoyan-I-A@users.noreply.github.com>
2020-02-21 14:43:12 -0500

    Ajout d'un autre code de changement d'adresses

---
+++ Code_pertinent/ToF_address_changer2.ino
@@ -0,0 +1,75 @@
+#include <Wire.h>
+#include <VL53L0X.h>
+#include <Adafruit_VL6180X.h>
+
+VL53L0X sensor1;
+VL53L0X sensor2;
+Adafruit_VL6180X sensorBleu;
+const int SENSOR_BLEU_SHUT_PIN = 11;
+const int SENSOR1_XSHUNT_PIN = 12;
+const int SENSOR2_XSHUNT_PIN = 13;
+const uint8_t SENSOR1_ADDRESS = 5;
+const uint8_t SENSOR2_ADDRESS = 6;
+
+void setSensorsAddress() {
+  // Changer l'addresse du sensor1
+  pinMode(SENSOR1_XSHUNT_PIN, INPUT);
+  delay(50);
+  sensor1.init(true);
+  delay(50);
+  sensor1.setAddress(SENSOR1_ADDRESS);
+  delay(50);
+
+
+  // Changer l'addresse du sensor2
+  pinMode(SENSOR2_XSHUNT_PIN, INPUT);
+  delay(50);
+  sensor2.init(true);
+  delay(50);
+  sensor2.setAddress(SENSOR2_ADDRESS);
+  delay(50);
+  Serial.println("Senseur verts changés.");
+  
+  // Activer le sensor bleu
+  pinMode(SENSOR_BLEU_SHUT_PIN, INPUT);
+  delay(50);
+  sensorBleu = Adafruit_VL6180X();
+  delay(50);
+  Serial.println("SensorBleu declare");
+  sensorBleu.begin();
+  delay(50);
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(SENSOR1_XSHUNT_PIN, OUTPUT);
+  pinMode(SENSOR2_XSHUNT_PIN, OUTPUT);
+  pinMode(SENSOR_BLEU_SHUT_PIN, OUTPUT);
+  digitalWrite(SENSOR1_XSHUNT_PIN, LOW);
+  digitalWrite(SENSOR2_XSHUNT_PIN, LOW);
+  digitalWrite(SENSOR_BLEU_SHUT_PIN, LOW);
+  Serial.begin(9600);
+  Wire.begin();
+  Serial.println("Start");
+  setSensorsAddress();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.print(sensor1.readRangeSingleMillimeters());
+  Serial.print(",");
+  Serial.print(sensor2.readRangeSingleMillimeters());
+  Serial.print(",");
+
+  float lux = sensorBleu.readLux(VL6180X_ALS_GAIN_5);
+  uint8_t range = sensorBleu.readRange();
+  uint8_t status = sensorBleu.readRangeStatus();
+
+  if (status == VL6180X_ERROR_NONE) {
+    Serial.println(range);
+  } else {
+    Serial.println(0);
+  }
+
+
+}
